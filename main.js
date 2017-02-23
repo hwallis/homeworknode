372a9bea76ec16fd2cf8179a792de1de34225d44
Heidi <hwallis@gmail.com>
2017-02-23 10:40:59 -0700

    done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("my first node app");
+var http = require('http');
+var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+
+var port = 8080;
+var logger = new EventEmitter();
+
+logger.on('error', function(message) {
+    console.log('ERROR: ' + message);
+});
+
+http.createServer( function(request, response) {
+  fs.readFile('index.html', function(err, content) {
+    if (err) {
+        response.writeHead(400, {'Content-Type': 'text/html'});
+        logger.emit('error', err);
+        response.end('failed');
+    } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(content);
+    }
+  })
+}).listen(port);
+
+console.log('server is running on local host: ' + port);
